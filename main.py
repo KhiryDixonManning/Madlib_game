51827318d89377aaf5c6b2b40131b6d181b655f5
KhiryDixonManning <Manningkhiry@gmail.com>
2024-12-07 14:45:33 -0600

    Basic bare bones, needs better story

---
+++ main.py
@@ -0,0 +1,14 @@
+#madlib game
+#game where you can create a story
+#fill in the blanks :)
+
+adjective1 = input("Enter an adjective(description): ")
+noun1 = input("Enter a noun(person, place, thing): ")
+adjective2 = input("Enter an adjective(description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective(description): ")
+
+print(f"One day, a {adjective1}{noun1} decided to go on an adventure.")
+print(f"It was feeling {adjective2} and wanted to {verb1} through the world")
+print(f"The {noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
